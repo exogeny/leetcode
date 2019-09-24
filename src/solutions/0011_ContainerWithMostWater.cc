d8e9ca7d14ea77585c1f2cf692b9e63d07d04101
Terence Wang <terence@culaer.com>
2019-09-24 21:23:35 +0800

    Add 11. Container With Most Water

---
+++ src/solutions/0011_ContainerWithMostWater.cc
@@ -0,0 +1,36 @@
+#include "solution.hpp"
+
+using std::vector;
+
+inline int max(int a, int b)
+{
+  return (a > b) ? a : b;
+}
+
+inline int min(int a, int b)
+{
+  return (a < b) ? a : b;
+}
+
+int Solution::maxArea(vector<int>& height)
+{
+  int result = 0, area;
+  vector<int>::iterator fp = height.begin();
+  vector<int>::iterator rp = height.end() - 1;
+
+  while (fp != rp)
+  {
+    area = min(*fp, *rp) * (rp - fp);
+    result = max(result, area);
+
+    if (*fp > *rp)
+    {
+      rp--;
+    }
+    else
+    {
+      fp++;
+    }
+  }
+  return result;
+}
---
+++ test/0011_ContainerWithMostWater.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+#include "solution.hpp"
+#include "support.hpp"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+using std::vector;
+
+TEST(Solution, maxArea)
+{
+  Solution s;
+
+  vector<int> input({1, 8, 6, 2, 5, 4, 8, 3, 7});
+  int result = s.maxArea(input);
+
+  EXPECT_EQ(49, result);
+}
+}
