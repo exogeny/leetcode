bc53bd6ee270cb7b4bbaabecb2f25c28d814c8e5
Terence Wang <terence@culaer.com>
2020-07-02 22:52:24 +0800

    Add the O(n) algorithm for 53. Maximum Subarray
    https://leetcode.com/problems/maximum-subarray/
    Runtime: 12 ms, faster than 76.93%
    Memory Usage: 13.4 MB, less than 22.48%

--- src/solutions/0053_MaximumSubarray.cc
+++ src/solutions/0053_MaximumSubarray.cc
@@ -2,39 +2,59 @@
 
 using std::vector;
 
-int maxSubArraywithIndex(vector<int> &nums, int left, int right)
+// using Divide-and-Conquer algorithm
+// Runtime: 12 ms, faster than 76.93%
+// Memory Usage: 13.2 MB, less than 63.55%
+static int maxSubArraywithIndex(vector<int> &nums, int left, int right)
 {
-    if (left == right)
-        return nums[left];
+  if (left == right)
+    return nums[left];
 
-    int i, sum;
-    int mid = (left + right) / 2;
-    int maxPadLeft = nums[mid], maxPadRight = nums[mid + 1];
-    int maxLeft = maxSubArraywithIndex(nums, left, mid);
-    int maxRight = maxSubArraywithIndex(nums, mid + 1, right);
-    for (i = mid, sum = 0; i >= left; i--)
-    {
-        sum += nums[i];
-        if (sum > maxPadLeft)
-            maxPadLeft = sum;
-    }
+  int i, sum;
+  int mid = (left + right) / 2;
+  int maxPadLeft = nums[mid], maxPadRight = nums[mid + 1];
+  int maxLeft = maxSubArraywithIndex(nums, left, mid);
+  int maxRight = maxSubArraywithIndex(nums, mid + 1, right);
+  for (i = mid, sum = 0; i >= left; i--)
+  {
+    sum += nums[i];
+    if (sum > maxPadLeft)
+      maxPadLeft = sum;
+  }
 
-    for (i = mid + 1, sum = 0; i <= right; i++)
-    {
-        sum += nums[i];
-        if (sum > maxPadRight)
-            maxPadRight = sum;
-    }
+  for (i = mid + 1, sum = 0; i <= right; i++)
+  {
+    sum += nums[i];
+    if (sum > maxPadRight)
+      maxPadRight = sum;
+  }
 
-    return MAX(MAX(maxLeft, maxRight), (maxPadLeft + maxPadRight));
+  return MAX(MAX(maxLeft, maxRight), (maxPadLeft + maxPadRight));
 }
 
+static int divideConquer(vector<int> &nums)
+{
+  if (nums.empty())
+    return 0;
+  if (nums.size() == 1)
+    return nums[0];
+
+  return maxSubArraywithIndex(nums, 0, nums.size() - 1);
+}
+
+// Runtime: 12 ms, faster than 76.93%
+// Memory Usage: 13.4 MB, less than 22.48%
 int Solution::maxSubArray(vector<int> &nums)
 {
-    if (nums.empty())
-        return 0;
-    if (nums.size() == 1)
-        return nums[0];
+  int result = nums[0], sum = nums[0];
+  for (int num : nums)
+  {
+    sum += num;
+    if (result < sum)
+      result = sum;
 
-    return maxSubArraywithIndex(nums, 0, nums.size() - 1);
+    if (sum < 0)
+      sum = 0;
+  }
+  return result;
 }
